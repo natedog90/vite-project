22b26c26b0eba3072e34ae73470983a3357d0b0d
nathan jimenez <njimenez@icstars.org>
2025-12-06 21:53:23 -0600

    Update base path to match new repository name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/myportfolio/", // Changed to match new repo name
+  base: "/portfolio/", // Changed to match new repo name
 });
